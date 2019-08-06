0843461cba71a6fdf54b4babf7c05b446beee570
proevan <yanghsin.lin@gmail.com>
2019-08-06 16:25:26 +0800

    [Home UI Mockup] handle background view double tap and long press

--- app/src/main/java/org/mozilla/rocket/home/HomeFragment.kt
+++ app/src/main/java/org/mozilla/rocket/home/HomeFragment.kt
@@ -1,15 +1,22 @@
 package org.mozilla.rocket.home
 
+import android.content.Context
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_home.home_background
 import org.mozilla.focus.R
 import org.mozilla.focus.locale.LocaleAwareFragment
 import org.mozilla.focus.navigation.ScreenNavigator
+import org.mozilla.focus.telemetry.TelemetryWrapper
 import org.mozilla.rocket.content.appComponent
 import org.mozilla.rocket.content.viewModelProvider
+import org.mozilla.rocket.theme.ThemeManager
 import javax.inject.Inject
 
 class HomeFragment : LocaleAwareFragment(), ScreenNavigator.HomeScreen {
@@ -18,6 +25,12 @@ class HomeFragment : LocaleAwareFragment(), ScreenNavigator.HomeScreen {
     lateinit var homeViewModelFactory: HomeViewModelFactory
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var themeManager: ThemeManager
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        themeManager = (context as ThemeManager.ThemeHost).themeManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         appComponent().inject(this)
@@ -27,6 +40,44 @@ class HomeFragment : LocaleAwareFragment(), ScreenNavigator.HomeScreen {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBackgroundView()
+    }
+
+    private fun initBackgroundView() {
+        themeManager.subscribeThemeChange(home_background)
+        val backgroundGestureDetector = GestureDetector(requireContext(), object : GestureDetector.SimpleOnGestureListener() {
+            override fun onDown(e: MotionEvent?): Boolean {
+                return true
+            }
+
+            override fun onDoubleTap(e: MotionEvent?): Boolean {
+                return homeViewModel.onBackgroundViewDoubleTap()
+            }
+
+            override fun onLongPress(e: MotionEvent?) {
+                homeViewModel.onBackgroundViewLongPress()
+            }
+        })
+        home_background.setOnTouchListener { _, event ->
+            backgroundGestureDetector.onTouchEvent(event)
+        }
+        homeViewModel.toggleBackgroundColor.observe(this, Observer {
+            val themeSet = themeManager.toggleNextTheme()
+            TelemetryWrapper.changeThemeTo(themeSet.name)
+        })
+        homeViewModel.resetBackgroundColor.observe(this, Observer {
+            themeManager.resetDefaultTheme()
+            TelemetryWrapper.resetThemeToDefault()
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        themeManager.unsubscribeThemeChange(home_background)
     }
 
     override fun getFragment(): Fragment = this
