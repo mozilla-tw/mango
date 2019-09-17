935260c798065b2fd584dc82633affa49af5fd52
proevan <yanghsin.lin@gmail.com>
2019-09-17 16:48:33 +0800

    [Debug Page] add toolbar to debug page

--- app/src/main/java/org/mozilla/rocket/debugging/DebugActivity.kt
+++ app/src/main/java/org/mozilla/rocket/debugging/DebugActivity.kt
@@ -2,9 +2,24 @@ package org.mozilla.rocket.debugging
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_debug.toolbar
+import org.mozilla.focus.R
 
 class DebugActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debug)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
 
     companion object {
         fun getStartIntent(context: Context) = Intent(context, DebugActivity::class.java)
