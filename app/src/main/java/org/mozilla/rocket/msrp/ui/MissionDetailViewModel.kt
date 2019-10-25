69440c9c08a9e0be8a1131811e6439ab70b0631c
proevan <yanghsin.lin@gmail.com>
2019-10-25 16:10:43 +0800

    fix coupon name on join mission content hub click onboarding

--- app/src/main/java/org/mozilla/rocket/msrp/ui/MissionDetailViewModel.kt
+++ app/src/main/java/org/mozilla/rocket/msrp/ui/MissionDetailViewModel.kt
@@ -75,7 +75,7 @@ class MissionDetailViewModel(
             startMissionReminder.value = mission
             refreshMissionsUseCase()
             if (isNeedJoinMissionOnboardingUseCase()) {
-                requestContentHubClickOnboardingUseCase(mission.title)
+                requestContentHubClickOnboardingUseCase(couponName = mission.description)
                 closeAllMissionPages.call()
             } else {
                 closePage.call()
